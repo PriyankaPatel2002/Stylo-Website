86a68790b521fc4f77c105fd07c7e6495767932a
Foodie Project <pinkpriyanka182@gmail.com>
2024-01-29 15:44:58 +0530

    My Stylo Website

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     alert("Welcome to script of jQuery");
+//     document.write("Welcome to script of jQuery");
+//     console.log("Welcome to script of jQuery");
+// });
+$(document).ready(function(){
+    $("#btn").click(function(){
+        $("#para").hide();
+    });
+    $("#btn1").click(function(){
+        $("#para").show();
+    });
+});
